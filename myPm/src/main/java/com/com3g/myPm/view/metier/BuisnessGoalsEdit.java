5934d54ceffb4aa50edbeeb8df3204bd5ffcbb18
Kecha mohamed <Kecha.mohamed@gmail.com>
2012-09-16 14:30:17 +0100

    workflow summary rating business goals

--- myPm/src/main/java/com/com3g/myPm/view/metier/BuisnessGoalsEdit.java
+++ myPm/src/main/java/com/com3g/myPm/view/metier/BuisnessGoalsEdit.java
@@ -11,26 +11,54 @@ import com.com3g.myPm.service.metier.BusinessGoalsService;
 @ManagedBean
 @ViewScoped
 public class BuisnessGoalsEdit implements Serializable {
-	
+
 	private BusinessGoals businessGoals = new BusinessGoals();
-	
+
 	private static BusinessGoalsService businessGoalsService() {
-        return SpringJSFUtil.getBean("businessGoalsService");
-    }
-	
-	public void confirm(){
-		
+		return SpringJSFUtil.getBean("businessGoalsService");
+	}
+
+	public void confirm() {
+
 		businessGoals.setStatus_Appraisal("Planning approuved by Manager");
 		businessGoalsService().saveBusinessGoals(businessGoals);
 	}
-	
-	public void deleteBuisnessGoal(){
-		
+
+	public void confirmSummary() {
+
+		businessGoals.setStatus_Appraisal("Summary approuved by Manager");
+		businessGoalsService().saveBusinessGoals(businessGoals);
+	}
+
+	public void confirmMidyear() {
+
+		businessGoals.setStatus_Appraisal("Mid year review approuved by Manager");
+		businessGoalsService().saveBusinessGoals(businessGoals);
+	}
+
+	public void deleteBuisnessGoal() {
+
 		businessGoalsService().delete(businessGoals);
 	}
-   
-	public void saveBuisnessGoal(){
-		
+
+	public void saveBuisnessGoal() {
+
+		businessGoalsService().saveBusinessGoals(businessGoals);
+	}
+
+	public void closeBuisnessGoal() {
+		businessGoals.setStatus_Appraisal("Closed");
+		businessGoalsService().saveBusinessGoals(businessGoals);
+	}
+
+	public void saveBuisnessGoalMidYear() {
+		businessGoals.setStatus_Appraisal("Mid year review submited by employe");
+		businessGoalsService().saveBusinessGoals(businessGoals);
+	}
+
+	public void saveBuisnessGoalSummary() {
+
+		businessGoals.setStatus_Appraisal("Summary submited by employe");
 		businessGoalsService().saveBusinessGoals(businessGoals);
 	}
 
@@ -41,8 +69,5 @@ public class BuisnessGoalsEdit implements Serializable {
 	public void setBusinessGoals(BusinessGoals businessGoals) {
 		this.businessGoals = businessGoals;
 	}
-	
-	
-	
 
 }
